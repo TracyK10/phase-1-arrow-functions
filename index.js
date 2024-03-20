cc6e67a1c12289a7957e5a0eba5d8e161fe1e0b6
Tracy Karanja <chirutracy@gmail.com>
2024-03-20 15:46:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(){
+    let a = 2000;
+    let b = 100
+    return a/b
+}
+
+const square = num => {
+    let x = 2
+    return x*x;
+}
+
+const add = (num2, num3) => {
+    let a = 3;
+    let b = 4;
+    return a+b
+}
